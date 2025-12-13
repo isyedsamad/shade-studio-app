a45222fdde4230e49fff0c0d8dc12acb1fb753d7
Syed Samad <syedsamad313@gmail.com>
2025-12-14 00:58:26 +0530

    fixing UI

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -2,7 +2,7 @@ import { useBg } from '@/context/BgContext';
 import React from 'react'
 
 const Card = (props) => {
-    const { setBg, setIsDarkMode } = useBg();
+    const { setBg, setIsDarkMode, isDarkMode } = useBg();
     const handleApply = () => {
         const raw = props.data.style_code;
         setBg(raw); // store in context
@@ -36,11 +36,11 @@ const Card = (props) => {
                     <h3 className='font-medium text-white text-xs'>copy code</h3>
                 </div>
             </div>
-            <div className='flex duration-300
+            <div className={`flex duration-300
                 opacity-100 visible group-hover:opacity-100 group-hover:visible
-                justify-start items-start flex-col px-4 py-3 bg-[var(--bg-primary)]/60 m-3 rounded-md'>
-                <h3 className='font-semibold text-[var(--text-primary)] text-sm'>{props.data.title}</h3>
-                <p className='font-medium text-[var(--text-secondary)] text-xs'>{props.data.description}</p>
+                justify-start items-start flex-col px-4 py-3 ${props.data.isDark ? 'bg-black/60' : isDarkMode ? 'bg-black/60' : 'bg-[var(--bg-primary)]/60'} m-3 rounded-lg`}>
+                <h3 className={`font-semibold ${props.data.isDark ? 'text-white' : 'text-[var(--text-primary)]'} text-sm`}>{props.data.title}</h3>
+                <p className={`font-medium ${props.data.isDark ? 'text-gray-200' : 'text-[var(--text-secondary)]'} text-xs`}>{props.data.description}</p>
             </div>
         </div>
     </div>
